488cc56eec0e4059e530098fe031a4a6165a6761
Ahmed Saber <ahmed0saber33@gmail.com>
2022-07-30 17:59:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const JSONexample = `
-    [
-        {
-            "path": "category1",
-            "name": "Category 1"
-        },
-        {
-            "path": "category2",
-            "name": "Category 2"
-        },
-        {
-            "path": "category3",
-            "name": "Category 3"
-        }
-    ]
-`
-document.write(JSONexample)
-console.log(JSON.parse(JSONexample))
+/* EMPTY */
